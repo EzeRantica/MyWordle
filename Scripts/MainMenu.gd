02428ba859d1622e4859f64396c8cdb54e795ff3
Rantica <ranticaezequiel@gmail.com>
2022-12-21 20:16:16 -0300

    Agregados cuadros de niveles en la sección de 'selección de nivel' y arregladas las transición de menús, empezado la lógica de fechas

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -4,38 +4,47 @@ func _ready() -> void:
 	$Main/StartGame.connect("pressed", self, "_on_StartGame_pressed")
 	$Main/SelectLevel.connect("pressed", self, "_on_SelectLevel_pressed")
 	$Main/Options.connect("pressed", self, "_on_Options_pressed")
+	$Options/BackButton.connect("pressed", self, "_on_BackFromOptions_pressed")
+	$SelectLevel/BackButton.connect("pressed", self, "_on_BackFromSelectLevel_pressed")
 	
 	####### CONEXIÓN CON LOS BOTONES DE NIVELES
-	$"SelectLevel/CenterHorizontalLevels/CenterVerticalLevels/Nivel1-Button".connect("pressed", self, "Nivel1Pressed")
-	$"SelectLevel/CenterHorizontalLevels/CenterVerticalLevels/Nivel2-Button".connect("pressed", self, "Nivel2Pressed")
+	$"SelectLevel/CenterHorizontalLevels/CenterVerticalLevels/LevelsRow1/Nivel1-Button".connect("pressed", self, "NivelPressed", [2022, 12, 20, "res://Scenes/Nivel1.tscn"])
+	$"SelectLevel/CenterHorizontalLevels/CenterVerticalLevels/LevelsRow1/Nivel2-Button".connect("pressed", self, "NivelPressed", [2023, 02, 02, "res://Scenes/Nivel2.tscn"])
 	pass
 
 
 func _on_StartGame_pressed() -> void:
+	$AnimationPlayer.play("FromMainToStartGame")
+	yield($AnimationPlayer, "animation_finished")
 	var _change = get_tree().change_scene("res://Scenes/Nivel1.tscn")
 
 
 func _on_SelectLevel_pressed() -> void:
-	MoveToSelectLevel()
-
+	$AnimationPlayer.play("FromMainToSelectLevel")
 
 func _on_Options_pressed() -> void:
-	pass
+	$AnimationPlayer.play("FromMainToOptions")
+
+func _on_BackFromOptions_pressed() -> void:
+	$AnimationPlayer.play("FromOptionsToMain")
+
+func _on_BackFromSelectLevel_pressed() -> void:
+	$AnimationPlayer.play("FromSelectLevelToMain")
 
 func NivelPressed(year : int, month : int, day : int, level : String):
 	var fechaActual = Time.get_date_string_from_system(false)
 	print("Año = " + fechaActual.substr(0, 4) + " // Mes = " + fechaActual.substr(5, 2) + " // Día = " + fechaActual.substr(8, 2))
 	
 	if (fechaActual.substr(0, 4)).to_int() < year:
+		print("No es " + String(year))
 		return
 	if (fechaActual.substr(5, 2)).to_int() < month:
+		print("No es Febrero")
 		return
 	if (fechaActual.substr(8, 2)).to_int() < day:
+		print("No es " + String(day) + " de Febrero")
 		return
 	
 	### Si pasa todos los bloqueos se cambia al nivel presionado
-	var _change = get_tree().change_scene("res://Scripts/Nivel1.gd")
-	
-func MoveToSelectLevel():
-	$AnimationPlayer.play("FromMainToSelectLevel")
-	pass
+	print("Changing scene to '" + level + "'")
+	var _change = get_tree().change_scene(level)
