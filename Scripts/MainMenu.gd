9d39c349322e659b51fad6e845b2573d2039bb10
Rantica <ranticaezequiel@gmail.com>
2023-01-11 20:25:49 -0300

    Terminada la lógica de bloqueo de niveles por día en febrero, cambiados los títulos de los niveles de 'Wordle' a 'Micarle', agregadas animaciones a los botones cuando no se puede seleccionar el nivel presionado

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -8,9 +8,21 @@ func _ready() -> void:
 	$SelectLevel/BackButton.connect("pressed", self, "_on_BackFromSelectLevel_pressed")
 	
 	####### CONEXIÓN CON LOS BOTONES DE NIVELES
-	$"SelectLevel/CenterHorizontalLevels/CenterVerticalLevels/LevelsRow1/Nivel1-Button".connect("pressed", self, "NivelPressed", [2022, 12, 20, "res://Scenes/Nivel1.tscn"])
-	$"SelectLevel/CenterHorizontalLevels/CenterVerticalLevels/LevelsRow1/Nivel2-Button".connect("pressed", self, "NivelPressed", [2023, 02, 02, "res://Scenes/Nivel2.tscn"])
-	pass
+	$"SelectLevel/CenterHorizontalLevels/CenterVerticalLevels/LevelsRow1/Nivel1-Button".connect("pressed", self, "NivelPressed", [2023, 01, 01, "res://Scenes/Nivel1.tscn", "Nivel1"])
+	$"SelectLevel/CenterHorizontalLevels/CenterVerticalLevels/LevelsRow1/Nivel2-Button".connect("pressed", self, "NivelPressed", [2023, 01, 02, "res://Scenes/Nivel2.tscn", "Nivel2"])
+	$"SelectLevel/CenterHorizontalLevels/CenterVerticalLevels/LevelsRow1/Nivel3-Button".connect("pressed", self, "NivelPressed", [2023, 01, 03, "res://Scenes/Nivel3.tscn", "Nivel3"])
+	$"SelectLevel/CenterHorizontalLevels/CenterVerticalLevels/LevelsRow1/Nivel4-Button".connect("pressed", self, "NivelPressed", [2023, 01, 04, "res://Scenes/Nivel4.tscn", "Nivel4"])
+	$"SelectLevel/CenterHorizontalLevels/CenterVerticalLevels/LevelsRow2/Nivel5-Button".connect("pressed", self, "NivelPressed", [2023, 01, 05, "res://Scenes/Nivel5.tscn", "Nivel5"])
+	$"SelectLevel/CenterHorizontalLevels/CenterVerticalLevels/LevelsRow2/Nivel6-Button".connect("pressed", self, "NivelPressed", [2023, 01, 06, "res://Scenes/Nivel6.tscn", "Nivel6"])
+	$"SelectLevel/CenterHorizontalLevels/CenterVerticalLevels/LevelsRow2/Nivel7-Button".connect("pressed", self, "NivelPressed", [2023, 01, 07, "res://Scenes/Nivel7.tscn", "Nivel7"])
+	$"SelectLevel/CenterHorizontalLevels/CenterVerticalLevels/LevelsRow2/Nivel8-Button".connect("pressed", self, "NivelPressed", [2023, 01, 08, "res://Scenes/Nivel8.tscn", "Nivel8"])
+	$"SelectLevel/CenterHorizontalLevels/CenterVerticalLevels/LevelsRow3/Nivel9-Button".connect("pressed", self, "NivelPressed", [2023, 01, 09, "res://Scenes/Nivel9.tscn", "Nivel9"])
+	$"SelectLevel/CenterHorizontalLevels/CenterVerticalLevels/LevelsRow3/Nivel10-Button".connect("pressed", self, "NivelPressed", [2023, 01, 10, "res://Scenes/Nivel10.tscn", "Nivel10"])
+	$"SelectLevel/CenterHorizontalLevels/CenterVerticalLevels/LevelsRow3/Nivel11-Button".connect("pressed", self, "NivelPressed", [2023, 01, 11, "res://Scenes/Nivel11.tscn", "Nivel11"])
+	$"SelectLevel/CenterHorizontalLevels/CenterVerticalLevels/LevelsRow3/Nivel12-Button".connect("pressed", self, "NivelPressed", [2023, 01, 12, "res://Scenes/Nivel12.tscn", "Nivel12"])
+	$"SelectLevel/CenterHorizontalLevels/CenterVerticalLevels/LevelsRow4/Nivel13-Button".connect("pressed", self, "NivelPressed", [2023, 01, 13, "res://Scenes/Nivel13.tscn", "Nivel13"])
+	$"SelectLevel/CenterHorizontalLevels/CenterVerticalLevels/LevelsRow4/Nivel14-Button".connect("pressed", self, "NivelPressed", [2023, 01, 14, "res://Scenes/Nivel14.tscn", "Nivel14"])
+####################################################################################################################################
 
 
 func _on_StartGame_pressed() -> void:
@@ -31,20 +43,34 @@ func _on_BackFromOptions_pressed() -> void:
 func _on_BackFromSelectLevel_pressed() -> void:
 	$AnimationPlayer.play("FromSelectLevelToMain")
 
-func NivelPressed(year : int, month : int, day : int, level : String):
+func NivelPressed(year : int, month : int, day : int, level : String, levelAnimationName : String):
 	var fechaActual = Time.get_date_string_from_system(false)
 	print("Año = " + fechaActual.substr(0, 4) + " // Mes = " + fechaActual.substr(5, 2) + " // Día = " + fechaActual.substr(8, 2))
 	
 	if (fechaActual.substr(0, 4)).to_int() < year:
 		print("No es " + String(year))
+		$LockedLevelAnimations.play("RESET")
+		yield($LockedLevelAnimations, "animation_finished")
+		$LockedLevelAnimations.play(levelAnimationName)
 		return
 	if (fechaActual.substr(5, 2)).to_int() < month:
 		print("No es Febrero")
+		$LockedLevelAnimations.play("RESET")
+		yield($LockedLevelAnimations, "animation_finished")
+		$LockedLevelAnimations.play(levelAnimationName)
 		return
 	if (fechaActual.substr(8, 2)).to_int() < day:
 		print("No es " + String(day) + " de Febrero")
+		$LockedLevelAnimations.play("RESET")
+		yield($LockedLevelAnimations, "animation_finished")
+		$LockedLevelAnimations.play(levelAnimationName)
 		return
 	
 	### Si pasa todos los bloqueos se cambia al nivel presionado
 	print("Changing scene to '" + level + "'")
 	var _change = get_tree().change_scene(level)
+
+
+func _on_LockedLevelAnimations_animation_finished(anim_name: String) -> void:
+	if anim_name != "RESET":
+		$LockedLevelAnimations.play("RESET")
